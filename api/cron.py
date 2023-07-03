edc529af787346487f225796f10be7f9f423cf46
Michael Bartholic <michaelbartholic@gmail.com>
2023-07-03 13:59:26 -0700

    Update cron.py with basic outline

---
+++ api/cron.py
@@ -0,0 +1,17 @@
+
+import datetime
+
+def main():
+    
+    # todo: generate auth header
+    
+    # get current user name
+    
+    # select next user name
+    
+    # post to endpoint 
+    
+    print(f"Cron has been invoked at {datetime.datetime.now()}")
+
+if __name__ == "__main__":
+    main()
