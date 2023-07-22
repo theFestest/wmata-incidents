37a0c6516254bd88285389e04f772e252a7384c7
theFestest <michaelbartholic@gmail.com>
2023-07-21 23:36:18 -0400

    Slight refactoring

--- api/cron.py
+++ api/cron.py
@@ -22,11 +22,15 @@ kv_client = None
 
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
+
+        main()
+
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write('Success!'.encode('utf-8'))
         return
 
 
